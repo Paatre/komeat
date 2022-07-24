13618d125d55512bc0a2d03c9484a6552ed490d5
Teemu Viikeri <teemu.viikeri@haltu.fi>
2022-07-24 23:50:29 +0300

    Use F() expression and redirect() in vote() view.

--- komeat/polls/views.py
+++ komeat/polls/views.py
@@ -22,4 +22,20 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        context = {'question': question,
+                   'error_message': "You didn't select a choice.", }
+        template = 'polls/detail.html'
+        return render(request, template, context)
+    else:
+        # Use F() expression to avoid race conditions
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return redirect(reverse('polls:results', args=(question.id,)))
