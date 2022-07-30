212e37114a93d68064cb480a50cdd0dd26079e4a
Teemu Viikeri <teemu.viikeri@haltu.fi>
2022-07-30 14:19:42 +0300

    Add MEDIA_ROOT to settings.py.

--- komeat/komeat/settings.py
+++ komeat/komeat/settings.py
@@ -120,6 +120,13 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = 'static/'
 
+
+# Media files (user-uploaded files)
+# https://docs.djangoproject.com/en/4.0/topics/files/
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
